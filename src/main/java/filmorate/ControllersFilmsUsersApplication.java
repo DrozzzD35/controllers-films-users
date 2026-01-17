8a91de52b11a2e55b2f4eab65c12dbaf8845aabf
DrozzzD35 <kapustin3595@yandex.ru>
2026-01-17 08:23:22 +0300

    feat: создать проект

---
+++ src/main/java/filmorate/ControllersFilmsUsersApplication.java
@@ -0,0 +1,13 @@
+package filmorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllersFilmsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllersFilmsUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/filmorate/ControllersFilmsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package filmorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllersFilmsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
