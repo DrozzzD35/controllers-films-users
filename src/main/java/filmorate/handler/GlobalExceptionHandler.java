45364ff464aa500119168a24ebdf47472480fa28
DrozzzD35 <kapustin3595@yandex.ru>
2026-01-20 20:02:11 +0300

    feat: написать глобальный обработчик исключений

---
+++ src/main/java/filmorate/handler/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package filmorate.handler;
+
+import filmorate.exception.NotFoundException;
+import filmorate.exception.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidation(final ValidationException e) {
+        log.error("Ошибка валидации: {}", e.getMessage());
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(final NotFoundException e) {
+        log.error("Объект не найден: {}", e.getMessage());
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNotReadable(final HttpMessageNotReadableException e) {
+        log.error("Ошибка даты релиза: {}", e.getMessage());
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNotValid(final MethodArgumentNotValidException e) {
+        log.error("Не достаточно данных: {}", e.getMessage());
+        return Map.of("error", "Не достаточно данных");
+    }
+
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    public Map<String, String> handleThrowable(final Throwable e) {
+//        log.error("Ошибка: {}", e.getMessage());
+//        return Map.of("error", e.getMessage());
+//    }
+
+
+}
